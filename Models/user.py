dd1d06d6adcf13a39f32395c0144372ef9403e85
anupriyab <anupriyab.kmb@gmail.com>
2024-03-13 07:43:26 +0100

    added ModelsforUserAPI

---
+++ Models/user.py
@@ -0,0 +1,9 @@
+class User:
+    def __init__(self, id, username, first_name, last_name, email, password, phone):
+        self.id = id
+        self.username = username
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+        self.phone = phone
